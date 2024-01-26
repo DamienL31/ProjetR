bf5c37e03d174c57c0450750bfee43aef7d711d6
Damien Lauger <152622955+DamienL31@users.noreply.github.com>
2024-01-26 16:48:34 +0100

    rajout graph et box

--- server.R
+++ server.R
@@ -38,37 +38,45 @@ server <- function(input, output, session) {
   
   
   output$numStreams <- renderValueBox({
-    valueBox(value = num_streams(), color = "olive",
-             subtitle = "Number of streams")
+    valueBox(value = num_streams(), color = "lime",
+             subtitle = "Number of streams",
+             icon = icon("headphones"))
   })
   
   output$numTracks <- renderValueBox({
   valueBox(value = num_tracks(), color = "olive",
-           subtitle = "Number of tracks")
+           subtitle = "Number of tracks",
+           icon = icon("music"))
   })
   
   
   output$numArtists <- renderValueBox({
     valueBox(value = num_artists(), color = "olive",
-             subtitle = "Number of Artists")
+             subtitle = "Number of Artists",
+             icon = icon("users"))
   
   })
   
   
   output$numGenre <- renderValueBox({
-    valueBox(value = num_genre(), color = "olive",
-             subtitle = "Number of Genre")
+    valueBox(value = num_genre(), color = "lime",
+             subtitle = "Number of Genre",
+             icon = icon("tag"))
     
   })
   
   
   output$datatable_track <- renderDT({
     data <- filtered_data() %>% 
-      ...
+      select(artist_name,track_name,streams,in_spotify_playlists,
+             in_apple_playlists, in_deezer_playlists) %>%
+      rename(artist = artist_name, track = track_name, spotify = in_spotify_playlists, 
+             apple = in_apple_playlists, deezer = in_deezer_playlists)%>%
+      arrange(desc(streams))
     
     datatable(data, options = list(
       scrollX = TRUE,
-      paginate = T,
+      paginate = TRUE,
       lengthMenu = c(5, 10, 15),
       pageLength = 20
     ))
@@ -78,10 +86,10 @@ server <- function(input, output, session) {
   output$tracksPerYearArtistPlot <- renderPlotly({
     
     data <- filtered_data() %>%
-      group_by(released_year) %>%
+      group_by(released_year, artist_name) %>%
       summarize(tracks = n())
     
-     ggplot(data, aes(x = released_year, y = tracks, legend = data$id_artists)) +
+     ggplot(data, aes(x = released_year, y = tracks, color = artist_name)) +
       geom_line() +
       labs(title = "Number of Tracks per Year",
            x = "Year",
@@ -94,13 +102,16 @@ server <- function(input, output, session) {
   output$genrePopularityPlot <- renderPlotly({
     
     data <- filtered_data() %>%
-      ...
+      group_by(artist_name, genre) %>%
+      summarize(count = n())
     
-    p <- ggplot()
-    ...
-    theme_minimal() 
-    
-    ggplotly(p)
+    ggplot(data, aes(x = genre, y = count, fill = genre)) +
+      geom_col() +
+      scale_fill_manual(values = rainbow(n_distinct(data$genre))) + 
+      labs(title = "Popularity of genre",
+           x = "Genre",
+           y = "Count") +
+      theme_classic()
     
   })
   
--- ui.R
+++ ui.R
@@ -2,12 +2,6 @@ base::source("packages.R")
 base::source("global.r")
 
 
-
-
-
-
-
-
 ui <- dashboardPage(skin = "black",
                     dbHeader,
                     dashboardSidebar(disable = TRUE),
@@ -33,34 +27,40 @@ ui <- dashboardPage(skin = "black",
                                     selected = "BTS", 
                                     choices = unique(spotify_data$artist_name))
                       ),
-                      
+                      box( title = "chat with chatGPT",
+                           solidHeader = FALSE, width = 6,
+                           column(3, align = "center"),
+                           column(6, align = "center",
+                                  textInput("message", "Your message:"),
+                                  actionButton("sendButton", "Send"),
+                                  textOutput("responseText")
+                           ),
+                           column(3, align = "center")),
                       box(
                         title = "Stats",
                         solidHeader = FALSE,
-                        width = 12,
-                        column(
-                          width = 4,
-                          valueBoxOutput("numStreams"),
-                          valueBoxOutput("numTracks"),
-                          valueBoxOutput("numArtists"),
-                          valueBoxOutput("numGenre")
-                        ),
-                        column(
-                          width = 6,
-                          plotlyOutput("tracksPerYearArtistPlot")
-                        )
+                        width = 6,
+                        valueBoxOutput("numStreams", width = 6),
+                        valueBoxOutput("numTracks", width = 6),
+                        valueBoxOutput("numArtists", width = 6),
+                        valueBoxOutput("numGenre", width = 6),
+                        br(),
+                        fluidRow(
+                          box(plotlyOutput("tracksPerYearArtistPlot")),
+                          box(plotlyOutput("genrePopularityPlot")
+                          ))),
+                      box(
+                        title = "Track Data",
+                        dataTableOutput("datatable_track")
                       )
-                      
-                      #                         br(),
-                      # #                         fluidRow(
-                      # #                          ...
-                      # #                         )
-                      # #                       ),
-                      # #                       br(),
-                      # #                       box(title = "Track & playlist presence",
-                      # #                           ...,
-                      # #                       )
-                      # #
-                    )
-)
+                    ))
+
+
+
+
+
+
+
+
+
 
